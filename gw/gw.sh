bf231ef2b0eaabf57ec3af6a6ece1c1de75aa5ab
Armen Baghumian <armen@opensourceclub.org>
2010-12-17 03:32:38 -0500

    Find a friendly name for file if possible

--- gw/gw.sh
+++ gw/gw.sh
@@ -8,7 +8,10 @@
 
 storage=~/.wallpapers/national_geographic
 url=http://photography.nationalgeographic.com/photography/photo-of-the-day/
-link=`wget -O /dev/stdout $url 2>/dev/null | sed -n '/Download Wallpaper/ !n; /Download Wallpaper/ { s/.*href="\([^"]\+\)">Download Wallpaper.*/\1/; p; q;}'`
+wget -O /tmp/document $url 2>/dev/null
+link=`cat /tmp/document | sed -n '/Download Wallpaper/ !n; /Download Wallpaper/ { s/.*href="\([^"]\+\)">Download Wallpaper.*/\1/; p; q;}'`
+caption=`cat /tmp/document | sed -n '/id="caption"/ !n; /<h2>/ !n; /<h2>/ { s/.*<h2>\([^<]\+\)<\/h2>/\1/; s/[- ,]/_/g; s/[\n\r]//g; p; q; }' | tr [:upper:] [:lower:]`
+rm -rf /tmp/document
 
 if [ ! -d $storage ]
 then
@@ -17,6 +20,12 @@ fi
 
 filepath=`echo "$link" | perl -MURI -le 'chomp($link = <>); print URI->new($link)->path'`
 filename=`basename "${filepath}"`
+
+if [ "${caption}x" != "x" ]
+then
+    extension=`echo "${filename}"| sed -e "s/.*\(\.[a-zA-Z]\+\)/\1/"`
+    filename="${caption}${extension}"
+fi
 
 if [ ! $storage/$filename ]
 then
