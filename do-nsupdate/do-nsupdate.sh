3a1ee503c96fb2f9ad58de8233655717dee9f64f
Armen Baghumian <armen@opensourceclub.org>
2011-01-25 21:59:54 +0330

    Changed url of echoip

--- do-nsupdate/do-nsupdate.sh
+++ do-nsupdate/do-nsupdate.sh
@@ -6,7 +6,7 @@ SERVER="209.9.227.207"
 ZONE="vardump.org"
 NTPSERVERS="ntp.belnet.be brussels.belnet.be leuven.belnet.be"
 HOSTNAME="home.vardump.org" #HOSTNAME="$(hostname | cut -d. -f1)"
-IPADDR=$(curl -s http://vardump.org/echoip.php)
+IPADDR=$(curl -s http://echoip.vardump.org)
 
 while (true);
 do
@@ -26,7 +26,7 @@ EOF
     while ( [ "x${OLDIP}" == "x${IPADDR}" ] );
     do
         sleep 5
-        NEWIP=$(curl -s http://vardump.org/echoip.php)
+        NEWIP=$(curl -s http://echoip.vardump.org/)
 
         if [ "x${NEWIP}" != "x" ] ; then
             IPADDR=$NEWIP
