473faf383dfef8a4ae767df75fb8bfaaa3a0903a
Armen Baghumian <armen@opensourceclub.org>
2011-07-26 12:04:09 +0430

    Check existance of bg

--- gw/gw-cron.sh
+++ gw/gw-cron.sh
@@ -17,6 +17,8 @@ redirect='/dev/null'
 # check the arguments, if debug is presented do the redirection to log file
 test "${1}" = 'debug' && redirect="${logfile}"
 
-/usr/bin/gw "${1}" | /usr/bin/xargs /usr/bin/feh --bg-fill >> $redirect 2>&1
+bg=$(/usr/bin/gw "${1}")
+
+[ -f "${bg}" ] && /usr/bin/feh --bg-fill $bg >> $redirect 2>&1
 
 exit 0
