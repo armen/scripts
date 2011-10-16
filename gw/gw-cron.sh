33113d1fc91662825221506fe11ba22980b65d7a
Armen Baghumian <armen@opensourceclub.org>
2011-10-16 22:18:53 +0330

    Pass the whole argument

--- gw/gw-cron.sh
+++ gw/gw-cron.sh
@@ -17,7 +17,7 @@ redirect='/dev/null'
 # check the arguments, if debug is presented do the redirection to log file
 test "${1}" = 'debug' && redirect="${logfile}"
 
-bg=$(/usr/bin/gw "${1}")
+bg=$(/usr/bin/gw $@)
 
 [ -f "${bg}" ] && /usr/bin/feh --bg-fill $bg >> $redirect 2>&1
 
